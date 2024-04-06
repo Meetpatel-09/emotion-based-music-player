6183b81085e162f4bbf9487184ff0c90a8f4da13
Meet Patel <pmeet007m@gmail.com>
2024-04-06 12:44:27 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, redirect, render_template, request, url_for, session
 from flask_bcrypt import Bcrypt
 from sqlalchemy import create_engine, text
+from werkzeug.utils import secure_filename
+import os
+import imghdr
+import datetime
+import base64
+import cv2
+from deepface import DeepFace
 
 app = Flask(__name__)
 
@@ -8,10 +15,10 @@ app.secret_key = "pyFace"
 
 bcrypt = Bcrypt(app)
 
-UPLOAD_FOLDER = "static/images"
+UPLOAD_FOLDER = "static/uploads"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-connection_string = "mysql+mysqlconnector://root:1234@localhost/test_db"
+connection_string = "mysql+mysqlconnector://root:1234@localhost/music_player"
 engin = create_engine(connection_string, echo=True)
 
 @app.route("/")
@@ -31,8 +38,6 @@ def login():
             result = conn.execute(text(query), {
                 "name": data["name"]
             })
-
-            print("row count ", result.rowcount)
 
             if result.rowcount == 1:
                 data1 = result.all();
@@ -55,12 +60,26 @@ def register():
     if request.method == 'POST':
         data = request.form
         with engin.connect() as conn:
-            query = "INSERT INTO accounts(name, password) VALUES (:name, :password)"
+
+            query = "SELECT * FROM accounts WHERE name = :name"
+
+            result = conn.execute(text(query), {
+                "name": data["username"]
+            })
+
+            print("row count ", result.rowcount)
+
+            if result.rowcount == 1:
+                return render_template('register.html', error="Username aleary taken")
+
+
+            query = "INSERT INTO accounts(name, birth_date, password) VALUES (:name, :birth_date, :password)"
 
             password = data['password']
             hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
             result = conn.execute(text(query), {
                 "name": data['username'],
+                "birth_date": data['birth_date'],
                 "password": hashed_password,
             })
             conn.commit()
@@ -75,5 +94,36 @@ def register():
     else:
         return "Requested Method Not Allowed"
 
+@app.route('/camera', methods=['POST', 'GET'])
+def camera():
+    if request.method == 'POST':
+        image_data_base64 = request.form['imageData']
+
+        image_data = base64.b64decode(image_data_base64.split(',')[1])
+
+        if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+            os.makedirs(app.config["UPLOAD_FOLDER"])
+
+        timestamp = datetime.datetime.now().timestamp() * 1000000
+        image_name = str(int(timestamp)) + '.png'
+        image_filename = os.path.join(app.config["UPLOAD_FOLDER"], image_name)
+
+        with open(image_filename, 'wb') as f:
+            f.write(image_data)
+
+        img = cv2.imread('static/uploads/1712342814765677.png')
+
+        result = DeepFace.analyze(img, actions = ['emotion'])
+        
+        emotion = result[0]['dominant_emotion']
+
+        print(emotion)
+
+        return render_template('camera.html', image_name=image_name, emotion=emotion)            
+
+    elif request.method == 'GET':
+        return render_template('camera.html')
+    else:
+        return "Requested Method Not Allowed"
 if __name__ == '__main__':
    app.run(debug=True)
